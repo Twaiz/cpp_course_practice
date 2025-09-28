48a7f3fbea7c6267d8bbe528741e5efd098a0efe
Twaiz <homenkobodya@gmail.com>
2025-09-28 22:10:32 +0300

    update[CompoundAssigmentOperators.cpp]: finish

--- C++ Course Practice/CompoundAssigmentOperators.cpp
+++ C++ Course Practice/CompoundAssigmentOperators.cpp
@@ -7,5 +7,17 @@ int CompoundAssigmentOperators() {
 	value += 5; // value = value + 5;
 	std::cout << "Value after += 5: " << value << std::endl;
 
+	value -= 5; // value = value - 5;
+	std::cout << "Value after -= 5: " << value << std::endl;
+
+	value *= 2; // value = value * 2;
+	std::cout << "Value after *= 2: " << value << std::endl;
+
+	value /= 3; // value = value / 3;
+	std::cout << "Value after /= 3: " << value << std::endl;
+
+	value %= 4; // value = value % 4;
+	std::cout << "Value after %= 4: " << value << std::endl;
+
 	return 0;
 }
