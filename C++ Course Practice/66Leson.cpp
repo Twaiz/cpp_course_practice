58d43a74d9f69bfee1ea8815106bdabdf9892aa4
Twaiz <homenkobodya@gmail.com>
2025-09-30 14:59:05 +0300

    update[66Leson.cpp]: improved and tested new moments

--- C++ Course Practice/66Leson.cpp
+++ C++ Course Practice/66Leson.cpp
@@ -3,8 +3,12 @@
 #include "9Modules.h"
 
 int LesonExersice() {
-	double A = 3*std::sqrt(3)/2*6.7;
-	std::cout << A << std::endl;
-	
+	double hexagon_area = ((3 * std::sqrt(3)) / 2) * (6.7 * 6.7);
+	int hexagon_area_int = std::round(hexagon_area);
+
+	std::cout << "Hexagon area (6.7 length): " << hexagon_area << std::endl;
+	std::cout << "Hexagon area (6.7 length - DEC): " << std::dec << hexagon_area_int << std::endl;
+	std::cout << "Hexagon area (6.7 length - OCT): " << std::oct << hexagon_area_int << std::endl;
+
 	return 0;
 }
