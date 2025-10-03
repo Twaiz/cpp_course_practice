261d425203de154d58f64f83c2b0cb385dbe6508
Twaiz <homenkobodya@gmail.com>
2025-10-03 17:39:00 +0300

    add[.clang-format]

--- C++ Course Practice/Constant.cpp
+++ C++ Course Practice/Constant.cpp
@@ -3,18 +3,18 @@
 
 int Constant() {
 
-    const int age{ 34 };
-    const float height{ 1.67f };
+	const int age{ 34 };
+	const float height{ 1.67f };
 
-    //age = 55; // Can't modify
-    //height = 1.8f;
+	//age = 55; // Can't modify
+	//height = 1.8f;
 
 
-    int years{ 3 * age };
+	int years{ 3 * age };
 
-    std::cout << "age : " << age << std::endl;
-    std::cout << "height : " << height << std::endl;
-    std::cout << "years : " << years << std::endl;
+	std::cout << "age : " << age << std::endl;
+	std::cout << "height : " << height << std::endl;
+	std::cout << "years : " << years << std::endl;
 
-    return 0;
+	return 0;
 }
