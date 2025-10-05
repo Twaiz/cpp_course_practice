09189b0cabec4c997d12bc4546a8d51b76cb23a2
Twaiz <homenkobodya@gmail.com>
2025-10-05 18:27:43 +0300

    update[TestShiftOperators.cpp]: added new tests

--- C++ Course Practice/TestShiftOperators.cpp
+++ C++ Course Practice/TestShiftOperators.cpp
@@ -21,5 +21,16 @@ int TestShiftOperators() {
     std::cout << data << std::endl;
     std::cout << std::bitset<16>(data) << std::endl;
 
+    data = {10};
+    data = static_cast<unsigned short int>(data << 2);
+    std::cout << data << std::endl;
+    std::cout << std::bitset<16>(data) << std::endl;
+
+    data = {10};
+    data = static_cast<unsigned short int>(
+        data >> 2);  // у нас выйдет 2, потому что 10 / 4 = 2.5, а дробная часть отбрасывается
+    std::cout << data << std::endl;
+    std::cout << std::bitset<16>(data) << std::endl;
+
     return 0;
 }
