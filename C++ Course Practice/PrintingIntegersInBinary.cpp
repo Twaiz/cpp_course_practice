8016e95691dd6041434e110cc1350149d663dbe5
Twaiz <homenkobodya@gmail.com>
2025-10-05 18:30:41 +0300

    add[PrintIntegerInBinary.cpp]

---
+++ C++ Course Practice/PrintingIntegersInBinary.cpp
@@ -0,0 +1,15 @@
+#include <bitset>
+#include <iostream>
+
+#include "12Module.h"
+
+int PrintIntegerInBinary() {
+    unsigned short int data{15};
+
+    std::cout << "data (dec) : " << std::showbase << std::dec << data << std::endl;
+    std::cout << "data (oct) : " << std::showbase << std::oct << data << std::endl;
+    std::cout << "data (hex) : " << std::showbase << std::hex << data << std::endl;
+    std::cout << "data (bin) : " << std::bitset<16>(data) << std::endl;
+
+    return 0;
+}
