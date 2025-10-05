f26a1a5b00c05035469061bf491c7698e980bb07
Twaiz <homenkobodya@gmail.com>
2025-10-05 18:10:01 +0300

    update[TestShipOperators.cpp]: updated logs

--- C++ Course Practice/TestShiftOperators.cpp
+++ C++ Course Practice/TestShiftOperators.cpp
@@ -5,11 +5,14 @@
 
 int TestShiftOperators() {
     unsigned short int data = {10};
-    std::cout << data;
+    std::cout << data << std::endl;
     std::cout << std::bitset<16>(data) << std::endl;
 
+    // >> Right shift - divides by 2^n
+    // >> Left shift - multiplies by 2^n
+
     data = static_cast<unsigned short int>(data >> 1);
-    std::cout << data;
+    std::cout << data << std::endl;
     std::cout << std::bitset<16>(data) << std::endl;
 
     return 0;
