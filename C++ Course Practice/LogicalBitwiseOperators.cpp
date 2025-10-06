e6cfe0c82d93c9e01879ca9e8d4a8644d96b6666
Twaiz <homenkobodya@gmail.com>
2025-10-06 18:36:44 +0300

    update[LogicalBitwiseOperators.cpp]: test bitwise operator - OR (|)

--- C++ Course Practice/LogicalBitwiseOperators.cpp
+++ C++ Course Practice/LogicalBitwiseOperators.cpp
@@ -9,7 +9,10 @@ int LogicalBitwiseOperators() {
 
     // value1 - 0000 1010 - bin
     // value2 - 0000 1111 - bin
+    
     // 0000 1010 - что будет при использовании логического оператора - AND (&)
+    // 0000 1111 - что будет при использовании логического оператора - OR (|)
+
 
     std::cout << "data1 (dec) : " << std::showbase << std::dec << data1 << std::endl;
     std::cout << "data1 (bin) : " << std::bitset<8>(data1) << std::endl;
@@ -19,6 +22,9 @@ int LogicalBitwiseOperators() {
 
     // AND (&)
     std::cout << "AND (&) : " << std::bitset<8>(data1 & data2) << std::endl;  // 00001010
+
+    // OR (|)
+    std::cout << "AND (&) : " << std::bitset<8>(data1 | data2) << std::endl;  // 00001010
 
     return 0;
 }
