e5daa0d4180631d7b28cfe393be07a778604b31b
Twaiz <homenkobodya@gmail.com>
2025-10-06 18:24:17 +0300

    add[LogicalBitwiseOperators.cpp]

---
+++ C++ Course Practice/LogicalBitwiseOperators.cpp
@@ -0,0 +1,15 @@
+#include <bitset>
+#include <iostream>
+
+int LogicalBitwiseOperators() {
+    unsigned char data1 = {10};
+    unsigned char data2 = {15};
+
+    std::cout << "data1 (dec) : " << std::showbase << std::dec << data1 << std::endl;
+    std::cout << "data1 (bin) : " << std::bitset<8>(data1) << std::endl;
+
+    std::cout << "data2 (dec) : " << std::showbase << std::dec << data2 << std::endl;
+    std::cout << "data2 (bin) : " << std::bitset<8>(data2) << std::endl;
+
+    return 0;
+}
