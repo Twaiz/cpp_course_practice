1baea3a733d505081da199f196a94377c23302a1
Twaiz <homenkobodya@gmail.com>
2025-10-06 19:36:23 +0300

    add[Masks.cpp]

---
+++ C++ Course Practice/Masks.cpp
@@ -0,0 +1,22 @@
+#include <bitset>
+#include <iostream>
+
+#include "12Module.h"
+
+int Masks() {
+    const unsigned char MASK1{0b00000001};  // Mask for bit 0
+    const unsigned char MASK2{0b00000010};  // Mask for bit 1
+    const unsigned char MASK3{0b00000100};  // Mask for bit 2
+    const unsigned char MASK4{0b00001000};  // Mask for bit 3
+    const unsigned char MASK5{0b00010000};  // Mask for bit 4
+    const unsigned char MASK6{0b00100000};  // Mask for bit 5
+    const unsigned char MASK7{0b01000000};  // Mask for bit 6
+    const unsigned char MASK8{0b10000000};  // Mask for bit 7
+
+    unsigned char value = {0b00000000};
+
+    value |= MASK2;  // Set bit 1
+    std::cout << "After setting bit 1: " << std::bitset<8>(value) << std::endl;
+
+    return 0;
+}
