ef95446e5ba39ae159af3ac5fc50f2752622ea01
Twaiz <homenkobodya@gmail.com>
2025-10-06 18:42:24 +0300

    update[LogicalBitwiseOperators.cpp]: test bitwise operator - XOR (^)

--- C++ Course Practice/LogicalBitwiseOperators.cpp
+++ C++ Course Practice/LogicalBitwiseOperators.cpp
@@ -12,19 +12,20 @@ int LogicalBitwiseOperators() {
     
     // 0000 1010 - что будет при использовании логического оператора - AND (&)
     // 0000 1111 - что будет при использовании логического оператора - OR (|)
+    // 0000 0101 - что будет при использовании логического оператора - XOR (^)
 
 
-    std::cout << "data1 (dec) : " << std::showbase << std::dec << data1 << std::endl;
     std::cout << "data1 (bin) : " << std::bitset<8>(data1) << std::endl;
-
-    std::cout << "data2 (dec) : " << std::showbase << std::dec << data2 << std::endl;
     std::cout << "data2 (bin) : " << std::bitset<8>(data2) << std::endl;
 
     // AND (&)
-    std::cout << "AND (&) : " << std::bitset<8>(data1 & data2) << std::endl;  // 00001010
+    std::cout << "AND (&) : " << std::bitset<8>(data1 & data2) << std::endl;  // 0000 1010
 
     // OR (|)
-    std::cout << "AND (&) : " << std::bitset<8>(data1 | data2) << std::endl;  // 00001010
+    std::cout << "OR (|) : " << std::bitset<8>(data1 | data2) << std::endl;  // 0000 1111
+
+    // XOR (^)
+    std::cout << "XOR (^) : " << std::bitset<8>(data1 ^ data2) << std::endl;  // 0000 0101
 
     return 0;
 }
