6379af66eed168998d626a932bc68e941519597e
Twaiz <homenkobodya@gmail.com>
2025-10-06 19:39:33 +0300

    update[LogicalBitwiseOperators.cpp]: fix error

--- C++ Course Practice/LogicalBitwiseOperators.cpp
+++ C++ Course Practice/LogicalBitwiseOperators.cpp
@@ -13,8 +13,8 @@ int LogicalBitwiseOperators() {
     // 0000 1010 - что будет при использовании логического оператора - AND (&)
     // 0000 1111 - что будет при использовании логического оператора - OR (|)
     // 0000 0101 - что будет при использовании логического оператора - XOR (^)
-    // data1 - 1111 0101 - что будет при использовании логического оператора - NOT (^)
-    // data2 - 1111 0000 - что будет при использовании логического оператора - NOT (^)
+    // data1 - 1111 0101 - что будет при использовании логического оператора - NOT (~)
+    // data2 - 1111 0000 - что будет при использовании логического оператора - NOT (~)
 
 
     std::cout << "data1 (bin) : " << std::bitset<8>(data1) << std::endl;
@@ -29,9 +29,9 @@ int LogicalBitwiseOperators() {
     // XOR (^)
     std::cout << "XOR (^) : " << std::bitset<8>(data1 ^ data2) << std::endl;  // 0000 0101
 
-    // NOT (^)
-    std::cout << "NOT (^) data1 : " << std::bitset<8>(~data1) << std::endl; // 1111 0101
-    std::cout << "NOT (^) data2 : " << std::bitset<8>(~data2) << std::endl; // 1111 0000
+    // NOT (~)
+    std::cout << "NOT (~) data1 : " << std::bitset<8>(~data1) << std::endl; // 1111 0101
+    std::cout << "NOT (~) data2 : " << std::bitset<8>(~data2) << std::endl; // 1111 0000
 
     return 0;
 }
