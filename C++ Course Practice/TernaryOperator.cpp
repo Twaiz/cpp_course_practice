337ebbf6e7c69063b6820039d3bf22e367a92991
Twaiz <homenkobodya@gmail.com>
2025-10-08 17:52:55 +0300

    update[TernaryOperator.cpp]: improved

--- C++ Course Practice/TernaryOperator.cpp
+++ C++ Course Practice/TernaryOperator.cpp
@@ -3,7 +3,11 @@
 int TernaryOperator() {
     int trueOrFalse = 10 > 15 ? 1 : 0;
     bool boolTrueOrFalse = trueOrFalse;
-    std::cout << std::boolalpha << boolTrueOrFalse << std::endl;
+    std::cout << std::boolalpha; 
+    std::cout << boolTrueOrFalse << std::endl;
+
+    auto number1 = 10 > 15 ? 2 : 3.5f;
+    std::cout << number1 << "----" << sizeof(number1) << std::endl;
 
 	return 0;
 }
