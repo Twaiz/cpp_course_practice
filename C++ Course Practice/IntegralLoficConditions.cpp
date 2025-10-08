95cfdad65695ddf30df5cdd202d7cae02994e716
Twaiz <homenkobodya@gmail.com>
2025-10-08 17:31:03 +0300

    add[IntegralLoficConditions.cpp]

---
+++ C++ Course Practice/IntegralLoficConditions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int IntegralLoficConditions() { 
+	if (10) {
+        std::cout << "True" << std::endl;
+    } else {
+        std::cout << "False" << std::endl;
+	}
+
+	if (-10) {
+		std::cout << "Oops" << std::endl;
+	} else {
+		std::cout << "Yeee" << std::endl;
+    }
+	
+	return 0; }
