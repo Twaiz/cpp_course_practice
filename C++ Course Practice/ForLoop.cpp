6b4b4e54b020ce5ca02bd4f65ae75fc3eeece14e
Twaiz <homenkobodya@gmail.com>
2025-10-09 13:13:54 +0300

    update[ForLoop.cpp]: improved code

--- C++ Course Practice/ForLoop.cpp
+++ C++ Course Practice/ForLoop.cpp
@@ -5,13 +5,15 @@ int ForLoop() {
     size_t value = {0};
 
     for (value; value <= max_value; value++) {
-        int value1 = value + 1;
         std::cout << value << '\n';
         
         if (value == max_value) {
-            std::cout << "All output : " << value1 << '\n';
+            std::cout << "All output : " << ++value << '\n';
         }
     }
+
+    std::cout << sizeof(size_t) << '\n';
+    std::cout << sizeof(unsigned int) << '\n';
 
     return 0;
 }
