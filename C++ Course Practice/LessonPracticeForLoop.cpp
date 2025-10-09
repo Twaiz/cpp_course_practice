1d5221891f3260eb429d37212a090b83af0a3f5b
Twaiz <homenkobodya@gmail.com>
2025-10-09 13:31:41 +0300

    update[LessonPracticeForLoop.cpp]: FINISH

--- C++ Course Practice/LessonPracticeForLoop.cpp
+++ C++ Course Practice/LessonPracticeForLoop.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int LessonPracticeForLoop() {
-    size_t COUNT = {99};
+    const size_t COUNT = {99};
     size_t START_VALUE = {0};
-    unsigned int sum = {0};
+    size_t sum = {0};
 
-    for (START_VALUE; START_VALUE < COUNT; START_VALUE++) {
+    for (START_VALUE; START_VALUE <= COUNT; START_VALUE++) {
         sum += START_VALUE;
         std::cout << "Current Value : " << START_VALUE << '\n';
     }
