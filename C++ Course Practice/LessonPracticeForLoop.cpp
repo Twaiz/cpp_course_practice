7be39f0a87318caaf57344401dc29583258c899f
Twaiz <homenkobodya@gmail.com>
2025-10-10 11:06:27 +0300

    update[LessonPracticeForLoop.cpp + Main.cpp]: test

--- C++ Course Practice/LessonPracticeForLoop.cpp
+++ C++ Course Practice/LessonPracticeForLoop.cpp
@@ -12,5 +12,13 @@ int LessonPracticeForLoop() {
 
     std::cout << "Sum Value : " << sum << '\n';
 
+    unsigned char ch = 'A';
+    std::cout << "Character : " << ch << " ASCII Value : " << static_cast<int>(ch) << '\n';
+
+    /*for (size_t i{101}; i <= 132; ++i) {
+        unsigned char ch = static_cast<unsigned char>(i);
+        std::cout << "ASCII Value : " << i << " Character : " << ch << '\n';
+    }*/
+
     return 0;
 }
--- C++ Course Practice/Main.cpp
+++ C++ Course Practice/Main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::cout << "Main Function - Start!" << std::endl;
 
-	ForLoopMultipleDeclarations();
+	LessonPracticeForLoop();
 
 	return 0;
 }
