c42bf15b0592cd0b6815f6fb7a9fb066c9c70500
Twaiz <homenkobodya@gmail.com>
2025-10-10 17:19:48 +0300

    update[LessonPracticeForLoop.cpp]: added Fourth Task

--- C++ Course Practice/LessonPracticeForLoop.cpp
+++ C++ Course Practice/LessonPracticeForLoop.cpp
@@ -21,7 +21,7 @@ int LessonPracticeForLoop() {
 
     for (size_t i{65}; i <= 90; ++i) {
         unsigned char ch = static_cast<unsigned char>(i);
-        
+
         if (i == 65) {
             allCh += ch;
             continue;
@@ -38,7 +38,6 @@ int LessonPracticeForLoop() {
     unsigned int numbers[] = {1, 21, 4, 5, 7, 3, 9, 3, 5, 17, 15};
     unsigned int sumNumbers = {0};
 
-
     for (auto number : numbers) {
         if (number % 3 == 0) {
             std::cout << "Number : " << number << '\n';
@@ -47,6 +46,22 @@ int LessonPracticeForLoop() {
     }
 
     std::cout << "All numbers : " << sumNumbers << '\n';
+
+    // FOURTH TASK
+
+    const unsigned int COUT_FOURTH = {1000};
+    unsigned int I_FOURTH = {100};
+    unsigned int sumFourth = {0};
+
+    do {
+        if (I_FOURTH % 19 == 0) {
+            sumFourth += 1;
+            std::cout << "Number : " << I_FOURTH << '\n';
+        }
+        ++I_FOURTH;
+    } while (I_FOURTH < COUT_FOURTH);
+
+    std::cout << "All numbers : " << sumFourth << '\n';
 
     return 0;
 }
