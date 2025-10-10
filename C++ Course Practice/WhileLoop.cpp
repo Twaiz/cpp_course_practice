85f12a14201fa0116506caf6bbd79dac96eb4a9e
Twaiz <homenkobodya@gmail.com>
2025-10-10 16:56:11 +0300

    add[WhileLoop.cpp]

---
+++ C++ Course Practice/WhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int WhileLoop() {
+    unsigned int i = {0};
+    unsigned int COUT = {10};
+
+    while (i < 10) {
+        std::cout << "Number : " << i << "/n";
+        ++i;
+    }
+
+    return 0;
+}
