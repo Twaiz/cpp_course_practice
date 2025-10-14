8ee63d56b43973a8da199f4ddcd5ee0818cbd8de
Twaiz <homenkobodya@gmail.com>
2025-10-14 15:33:04 +0300

    add[InfiniteLoopsPractice.cpp]

---
+++ C++ Course Practice/InfiniteLoopsPractice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int InfiniteLoopsPractice() {
+    int number1 = {0};
+    int number2 = {0};
+    unsigned char operand = {'+'};
+    
+    std::cout << operand;
+
+    return 0;
+}
