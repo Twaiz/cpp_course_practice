2dcf7de25f2139808f06950bb565bf3c1cbf9dc7
Twaiz <homenkobodya@gmail.com>
2025-10-15 17:07:35 +0300

    update[DeclaringAndUsingArrays.cpp]

--- C++ Course Practice/DeclaringAndUsingArrays.cpp
+++ C++ Course Practice/DeclaringAndUsingArrays.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
 int DeclaringAndUsingArrays() {
+    /*
     int numbers1[5];
+    numbers1[0] = 5;
+    numbers1[1] = 10;
 
     for (int number : numbers1) {
         std::cout << number << std::endl;
     }
+    */
+
+    /*
+    int numbers2[] = {1, 2, 3};
+    for (int number1{0}; number1 < 3; ++number1) {
+        std::cout << numbers2[number1] << std::endl;
+    }
+    */
+
+
 
     return 0;
 }
