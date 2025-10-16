06bb61051654e0e6120d62f1fabf27a3a8331930
Twaiz <homenkobodya@gmail.com>
2025-10-16 21:09:20 +0300

    add[GeneratingRandomNumbers.cpp]

---
+++ C++ Course Practice/GeneratingRandomNumbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int GeneratingRandomNumbers() {
+    int randomNumber1 = std::rand();
+
+    std::cout << "Random Number 1 : " << randomNumber1 << std::endl;
+
+    return 0;
+}
