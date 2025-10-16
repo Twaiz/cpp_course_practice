e4a3dc4d04d0c0135fcffbe4487a515692ab7c16
Twaiz <homenkobodya@gmail.com>
2025-10-16 21:18:04 +0300

    update[GeneratingRandomNumbers.cpp]: FINISH

--- C++ Course Practice/GeneratingRandomNumbers.cpp
+++ C++ Course Practice/GeneratingRandomNumbers.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 int GeneratingRandomNumbers() {
-    int randomNumber1 = std::rand();
+    std::srand(std::time(0));
 
+    int randomNumber1 = std::rand() % 16; // 0 --- 15
+    int randomNumber2 = std::rand() % 15 + 1;  // 1 --- 15
+    
     std::cout << "Random Number 1 : " << randomNumber1 << std::endl;
+    std::cout << "Random Number 2 : " << randomNumber2 << std::endl;
 
     return 0;
 }
