52ff9eac859237d92115a5ff3e2ad90bc7925f31
Twaiz <homenkobodya@gmail.com>
2025-10-16 20:52:51 +0300

    add[BoundsArray.cpp]

---
+++ C++ Course Practice/BoundsArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int BoundsArray() {
+    int numbers[] = {17, 46546, 798, 1231, 4657, 465};
+    unsigned int index = 0;
+
+    for (auto i : numbers) {
+        std::cout << "Element : " << i << std::endl;
+        std::cout << "Index : " << index << std::endl;
+        ++index;
+    }
+
+    // What if we access out of bound element
+    std::cout << "Accessing out of bound element : " << numbers[10] << std::endl;
+    numbers[10] = 100;  // Writing to out of bound element
+    std::cout << "After writing to out of bound element : " << numbers[10] << std::endl;
+
+    return 0;
+}
